1c2cf928312e2f978dab41229047fed61f196ac8
Santiago <Santiago@example.com>
2025-11-23 22:22:24 -0500

    se hace gridsearch para extra tree

--- train_model.py
+++ train_model.py
@@ -50,12 +50,14 @@ def train_and_save():
     print(X_train)
 
     model = ExtraTreesRegressor(
-        n_estimators=150,     # menos árboles (antes 300/1000)
-        max_depth=12,        # límite de profundidad para reducir tamaño
-        max_features=0.4,    # como en tu búsqueda de hiperparámetros
-        min_samples_leaf=2,  # hojas con algo más de datos → árboles menos grandes
+        n_estimators=200,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        max_features='sqrt',
+        max_depth=30,
+        bootstrap=False,
         random_state=42,
-        n_jobs=-1,
+        n_jobs=-1
     )
 
     model.fit(X_train, y_train)
